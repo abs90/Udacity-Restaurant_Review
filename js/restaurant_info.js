29ac2ec24db7019b22a921d6b9ae5e13c769ffdc
Abs90 <absalb96@gmail.com>
2019-07-15 20:30:28 +0100

    Service worker added
    Problem with sw

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1IjoiYWJzOTAiLCJhIjoiY2p4OHppaXY0MDgwZTNvcGsyYml3bTNubSJ9.aop2BxlfOhBJBOHg4CuK2Q',
+        mapboxToken: 'pk.eyJ1IjoiYWJzOTAiLCJhIjoiY2p4OTRqNWl1MHBzZzQxanV1ZzVlcXdpNSJ9.whi9zhud2ryeJkCGPd3yjA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
